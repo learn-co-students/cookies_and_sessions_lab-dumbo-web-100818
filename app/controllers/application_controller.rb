079b99b5191af898100c118f41e037d11b3a5053
CarsonRey <c.rey494@gmail.com>
2018-11-11 17:40:44 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :cart
+  #helper_method gives other that directly/indirectly inherit from this ApplicationController
 
   def cart
     cart = session[:cart] || []
